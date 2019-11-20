b8418ca4cb3af7ad7c50528430c38c96e138cf4e
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-11-20 17:35:26 +0530

    Styling Changes

--- src/CityAQI.js
+++ src/CityAQI.js
@@ -11,44 +11,32 @@ const CityAQI = props => {
     const uid = props.cityInfo.uid;
 
     const getCategorizedAQI = aqi => {
-        let bColor = '#ebebeb';
-        let fColor = '#000000';
+        let className = 'unknown';
         let impact = 'Unknown';
 
         if (aqi >= 0 && aqi <= 50) {
             impact = 'Good';
-            bColor = '#52B947';
-            fColor = '#000000';
+            className = 'good';
         } else if (aqi >= 51 && aqi <= 100) {
             impact = 'Moderate';
-            bColor = '#F3EC19';
-            fColor = '#000000';
+            className = 'moderate';
         } else if (aqi >= 101 && aqi <= 150) {
             impact = 'Unhealthy for Sensitive Groups';
-            bColor = '#F57E20';
-            fColor = '#FFFFFF';
+            className = 'unhealthy-sentitive';
         } else if (aqi >= 151 && aqi <= 200) {
             impact = 'Unhealthy';
-            bColor = '#ED1D24';
-            fColor = '#FFFFFF';
+            className = 'unhealthy';
         } else if (aqi >= 201 && aqi <= 300) {
             impact = 'Very Unhealthy';
-            bColor = '#7E2B7D';
-            fColor = '#FFFFFF';
+            className = 'very-unhealthy';
         } else if (aqi >= 301) {
             impact = 'Hazardous';
-            bColor = '#480D27';
-            fColor = '#FFFFFF';
+            className = 'hazardous';
         }
-        let style = {
-            backgroundColor: bColor,
-            color: fColor,
-            borderColor: bColor
-        };
-
+        
         let catagorized = {};
         catagorized['impact'] = impact;
-        catagorized['style'] = style;
+        catagorized['className'] = className;
 
         console.log(catagorized);
 
@@ -59,9 +47,8 @@ const CityAQI = props => {
         return `${new Date(atTime).getHours()}:${new Date(atTime).getMinutes()}:${new Date(atTime).getSeconds()}`
     }
     return (
-        <div 
-            style={ getCategorizedAQI(aqi).style } 
-            className='cityInfo'
+        <div  
+            className={`cityInfo ${getCategorizedAQI(aqi).className}`}
             onClick={ () => setShowDetails(!showDetails)}>
             <span>At { getAtTimeFormatted(atTime) }: { placeName } - { aqi }</span>
             <div><b>{ getCategorizedAQI(aqi).impact }</b></div>
